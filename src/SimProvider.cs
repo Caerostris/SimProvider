c7b109e88d1014f6868c18ebdc8b22815f2ab08e
Keno Schwalb <contact@caerostris.com>
2013-04-16 13:47:16 +0200

    made DataSet static so that it can be used from a DLL

--- src/SimProvider.cs
+++ src/SimProvider.cs
@@ -63,7 +63,7 @@ namespace SimProvider
          * I choose the second one for now because I just like it more, any thoughts on this?
          */
 
-        public DataSet getNextStep(DataSet dataset)
+        public static DataSet getNextStep(DataSet dataset)
         {
             double forceMoving = (dataset.bike.engine.torque * dataset.bike.trs * dataset.bike.tra * 0.9) / (0.5 * dataset.bike.tire.diameter); // 0.9 is some factor that I can't remember, 0.5 is a factor in formula
             double forceStopping = (0.5 * 0.57 * 1.2041 * dataset.bike.surface * dataset.velocity) + (dataset.bike.tire.crr * dataset.bike.weight * 9.81); // 0.57 is the coefficient of flow resistance, 1.2401 is the density of air, 9.81 is obvious
